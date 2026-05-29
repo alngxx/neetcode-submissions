fa74fbb8b2b659dbf56afc743889264653086a88
Nguyen An Loc <133493885+alngxx@users.noreply.github.com>
2026-05-29 14:21:55 +0700

    Add: find-k-closest-elements - submission-0

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        """ Two Pointers: O(n-k)
+        Since the array is sorted, can use two pointers to find the range of k elements
+        """
+        l, r = 0, len(arr) - 1
+
+        # While loop break, keep the range exactly = k
+        while r - l >= k:
+            if abs(x - arr[l]) > abs(x - arr[r]):
+                l += 1
+            else: 
+                r -= 1
+        
+        return arr[l : r+1]
+
+        
+
+
+        
