9c48aff28a5515d0a0b40caca8a567b14b23566b
Nguyen An Loc <133493885+alngxx@users.noreply.github.com>
2026-06-08 13:57:54 +0700

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)   # k search range: min speed = 1, max speed = largest pile
+        res = r                # worse case: max speed
+
+        while l <= r:
+            mid = (l + r) // 2 # try mid speed
+
+            total_time = 0     # total eating time
+            for p in piles:
+                total_time += math.ceil(p / mid)  # ceil, not floor: k = 2, need 2 hours to eat 5 piles, not 1
+            
+            # if total eating time <= h: satisfy, update res
+            if total_time <= h:
+                res = mid
+                r = mid - 1     # try smaller, minimiz k
+            else:
+                l = mid + 1     # if not satisfy, search larger k
+
+        return res              # return min valid k
