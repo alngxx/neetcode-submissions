0cad24b9717632e426318d3f12683ff34b4a9b38
Nguyen An Loc <133493885+alngxx@users.noreply.github.com>
2026-06-14 18:04:19 +0700

    Add: add-two-numbers - submission-2

---
+++ Data Structures & Algorithms/add-two-numbers/submission-2.py
@@ -0,0 +1,32 @@
+""" Linked List store numbers in reverse order, thus first nodes are the last digits
+Thus, calculation is straightforward:
+1. Add two digits from l1 and l2
+2. Add carry from previous step
+3. Save result digit = sum % 10 as new node
+4. Update carry = sum // 10
+5. Move pointers to next nodes
+"""
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()      # dummy.next = result list
+        cur = dummy
+        carry = 0
+
+        # loop until both lists = None or no carry remains
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            # calculate new digit and carry
+            sum = v1 + v2 + carry
+            carry = sum // 10
+            val = sum % 10
+            cur.next = ListNode(val)    # record new digit
+
+            # move to next digits in both lists
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        
+        return dummy.next
