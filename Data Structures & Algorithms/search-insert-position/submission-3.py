b568e21dfaaf63b55d5265b46175e1f04b19a39d
Nguyen An Loc <133493885+alngxx@users.noreply.github.com>
2026-07-26 09:26:40 +0700

    Add: search-insert-position - submission-3

---
+++ Data Structures & Algorithms/search-insert-position/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        # Loop stops when left > right while can't find target
+        while left <= right:
+            mid = left + (right-left) // 2
+
+            if nums[mid] == target:
+                return mid
+            
+            elif nums[mid] > target:
+                right = mid - 1
+            
+            else:
+                left = mid + 1
+
+        # Now left > right, or nums[left] > target > nums[right]
+        # Thus, left = inserted index i.e. insert after right
+        return left
+        
+        
